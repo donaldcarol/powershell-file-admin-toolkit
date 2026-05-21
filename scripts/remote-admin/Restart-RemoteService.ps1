1a0b2c2185a2ec3cb96293377730ecace4623e20
DonaldCarol <donald@dakini.network>
2026-05-21 19:11:06 +0300

    added more files \remote-admin

--- scripts/remote-admin/Restart-RemoteService.ps1
+++ scripts/remote-admin/Restart-RemoteService.ps1
@@ -1,6 +1,38 @@
-param($ComputerName, $ServiceName)
+<#
+.SYNOPSIS
+Restart a Windows service on one or more remote computers.
+#>
 
-Invoke-Command -ComputerName $ComputerName -ScriptBlock {
-    param($ServiceName)
-    Restart-Service $ServiceName
-} -ArgumentList $ServiceName
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+    [Parameter(Mandatory = $true)]
+    [string[]]$ComputerName,
+
+    [Parameter(Mandatory = $true)]
+    [string]$ServiceName
+)
+
+foreach ($computer in $ComputerName) {
+    try {
+        Write-Host "Processing $computer..."
+
+        if ($PSCmdlet.ShouldProcess($computer, "Restart service $ServiceName")) {
+            Invoke-Command -ComputerName $computer -ScriptBlock {
+                param($ServiceName)
+
+                Restart-Service -Name $ServiceName -ErrorAction Stop
+
+                Get-Service -Name $ServiceName |
+                Select-Object Name, Status, MachineName
+            } -ArgumentList $ServiceName
+        }
+    }
+    catch {
+        [PSCustomObject]@{
+            ComputerName = $computer
+            ServiceName  = $ServiceName
+            Status       = "Error"
+            ErrorMessage  = $_.Exception.Message
+        }
+    }
+}
