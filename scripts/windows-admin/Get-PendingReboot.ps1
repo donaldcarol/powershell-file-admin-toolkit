90cc88c7938de73a6b71d04dae824a2320156db0
Donald Carol Fitai <donald@dakini.network>
2026-05-24 11:56:41 +0300

    Add pending reboot check to Get-PendingReboot.ps1
    Check for pending reboots in the system registry and return a custom object with the computer name and reboot status.

--- scripts/windows-admin/Get-PendingReboot.ps1
+++ scripts/windows-admin/Get-PendingReboot.ps1
@@ -1,1 +1,12 @@
-Get-Process | Sort-Object CPU -Descending | Select-Object -First 5 Name, CPU
+$reboot=
+
+Test-Path `
+"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending"
+
+[PSCustomObject]@{
+
+ComputerName=$env:COMPUTERNAME
+
+PendingReboot=$reboot
+
+}
