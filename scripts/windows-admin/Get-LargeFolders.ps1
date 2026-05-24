b213bfd4a9d58af855c04beaf1ab81eb91a7698a
Donald Carol Fitai <donald@dakini.network>
2026-05-24 11:57:25 +0300

    Add script to list large folders by size

--- scripts/windows-admin/Get-LargeFolders.ps1
+++ scripts/windows-admin/Get-LargeFolders.ps1
@@ -1,1 +1,33 @@
-Get-Process | Sort-Object CPU -Descending | Select-Object -First 5 Name, CPU
+param(
+[string]$Path="C:\"
+)
+
+Get-ChildItem `
+$Path `
+-Directory |
+
+ForEach-Object {
+
+[PSCustomObject]@{
+
+Folder=$_.Name
+
+SizeGB=[math]::Round(
+
+(
+Get-ChildItem `
+$_.FullName `
+-File `
+-Recurse `
+-ErrorAction SilentlyContinue |
+
+Measure-Object Length -Sum
+).Sum/1GB
+
+,2)
+
+}
+
+} |
+
+Sort-Object SizeGB -Descending
