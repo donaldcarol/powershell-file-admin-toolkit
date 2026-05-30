f74e24b380a9a26834290c8e32f07e7afbcaeb43
Donald Carol Fitai <donald@dakini.network>
2026-05-30 13:40:55 +0300

    Update Export-ADUsers.ps1

--- scripts/ad-admin/Export-ADUsers.ps1
+++ scripts/ad-admin/Export-ADUsers.ps1
@@ -1,1 +1,30 @@
-Get-Process | Sort-Object CPU -Descending | Select-Object -First 5 Name, CPU
+[CmdletBinding()]
+param(
+    [string]$SearchBase,
+    [string]$ExportCsv = ".\reports\ad-users-report.csv"
+)
+
+Import-Module ActiveDirectory
+
+$params = @{
+    Filter = "*"
+    Properties = @(
+        "Department",
+        "Title",
+        "Mail",
+        "Enabled",
+        "LastLogonDate",
+        "PasswordLastSet"
+    )
+}
+
+if ($SearchBase) {
+    $params.SearchBase = $SearchBase
+}
+
+$results = Get-ADUser @params |
+Select-Object Name, SamAccountName, Enabled, Mail, Department, Title, LastLogonDate, PasswordLastSet |
+Sort-Object Name
+
+$results | Export-Csv $ExportCsv -NoTypeInformation
+$results
