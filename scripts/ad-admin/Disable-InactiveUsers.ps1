c4c6c391cbe68887086ae664d4aabf885dabefdd
Donald Carol Fitai <donald@dakini.network>
2026-05-30 13:41:22 +0300

    Update Disable-InactiveUsers.ps1

--- scripts/ad-admin/Disable-InactiveUsers.ps1
+++ scripts/ad-admin/Disable-InactiveUsers.ps1
@@ -1,1 +1,24 @@
-Get-Process | Sort-Object CPU -Descending | Select-Object -First 5 Name, CPU
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(
+    [int]$DaysInactive = 180,
+    [string]$ExportCsv = ".\reports\users-to-disable.csv"
+)
+
+Import-Module ActiveDirectory
+
+$cutoff = (Get-Date).AddDays(-$DaysInactive)
+
+$users = Get-ADUser -Filter {
+    Enabled -eq $true -and LastLogonDate -lt $cutoff
+} -Properties LastLogonDate |
+Select-Object Name, SamAccountName, DistinguishedName, LastLogonDate
+
+$users | Export-Csv $ExportCsv -NoTypeInformation
+
+foreach ($user in $users) {
+    if ($PSCmdlet.ShouldProcess($user.SamAccountName, "Disable AD user")) {
+        Disable-ADAccount -Identity $user.DistinguishedName
+    }
+}
+
+$users
